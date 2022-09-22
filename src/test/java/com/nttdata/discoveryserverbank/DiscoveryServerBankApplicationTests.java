7a6be2d22cb2ea0797f5cfc86dfb5b396347107f
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-09-22 01:31:27 -0500

    add estructura base

---
+++ src/test/java/com/nttdata/discoveryserverbank/DiscoveryServerBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.discoveryserverbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
